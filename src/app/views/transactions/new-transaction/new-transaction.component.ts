f53efcdfbfa5c214f0f4532c3280cfb97b4afb25
albjongjuzi <albjon.gjuzi@wearefiber.com>
2021-01-04 17:56:57 +0100

    store dashboard after build

--- src/app/views/transactions/new-transaction/new-transaction.component.ts
+++ src/app/views/transactions/new-transaction/new-transaction.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-new-transaction',
   templateUrl: './new-transaction.component.html',
-  styleUrls: ['./new-transaction.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewTransactionComponent implements OnInit {
