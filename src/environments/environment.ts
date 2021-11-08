d6c79a1310d9970e0ef13246404606e2c5d6e0d3
gjuzialbjon <albjon.gjuzi@gmail.com>
2021-11-08 19:58:54 +0100

    update path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	API_URL: '://mappo.wearefiber.com/',
+	API_URL: '://35.197.192.204:30001/',
 	theme: 'corporate',
 	btc_store_id: '603d48daeab0d70008bbc19f',
 	unknown_client_id: '6093b08678a78e00085bad9a'
